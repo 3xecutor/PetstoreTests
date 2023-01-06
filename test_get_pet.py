12f865f1d3969d6171c4e3dbd08c1cf79a587749
Your Name <you@example.com>
2023-01-06 18:44:22 +0500

    final fix

--- test_get_pet.py
+++ test_get_pet.py
@@ -3,6 +3,7 @@ import requests
 
 import data
 
+
 @pytest.fixture
 def pet_id():
     return 450198289
@@ -23,10 +24,10 @@ def expected_pet_data(pet_id):
     return {"id": pet_id, "name": "gold", "status": "available"}
 
 
-@pytest.mark.parametrize("pet_id,expected_pet_data", [(450198289,
-                                                       hh.get_pet)])
+@pytest.mark.parametrize("pet_id,expected_pet_data", [(450198289, data.get_pet)])
 def test_get_pet_by_id(pet_id, pet_url, pet_headers, expected_pet_data):
     response = requests.get(pet_url, headers=pet_headers)
     assert response.status_code == 200
     response = requests.get(pet_url, headers=pet_headers)
     assert response.json() == expected_pet_data
+    
